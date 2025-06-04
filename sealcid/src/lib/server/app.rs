ac440783219410849f3e648692f4b80c7ecf004b
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-06-04 15:08:30 +0200

    fix:configure interface
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- sealcid/src/lib/server/app.rs
+++ sealcid/src/lib/server/app.rs
@@ -7,19 +7,19 @@ pub trait App {
     /// It should store a thread handler like `JoinHandle` or similar
     /// It should not block the current thread.
     fn run(&mut self) -> impl Future<Output = Result<(), Error>>;
-    
+
     /// Configures the application with the provided configuration.
     fn configure<T>(&mut self, config: T) -> impl Future<Output = Result<(), Error>>;
-    
+
     /// Stops the application by aborting the running thread or process.
     fn stop(&mut self) -> impl Future<Output = Result<(), Error>>;
-    
+
     /// Those methods are used to get the state of the application.
     /// This should return a displayable configuration. (Not the secret !!!)
-    fn configuration<T>(&self, config: T) -> Result<impl Display, Error>;
-    
+    fn configuration(&self) -> Result<impl Display, Error>;
+
     /// Returns the current status of the application.
     fn status(&self) -> impl Future<Output = Result<Status, Error>>;
-    
+
     fn name(&self) -> String;
 }
