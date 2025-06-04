089d91aed158694a8e61f7e34a1bc17c88050539
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-06-04 13:52:47 +0200

    feat:lib structure
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

---
+++ sealcid/src/bin/sealcid/main.rs
@@ -0,0 +1,12 @@
+use sealcid::server::app::App;
+
+fn main() {
+    let _ = App {
+        name: todo!(),
+        version: todo!(),
+        description: todo!(),
+        author: todo!(),
+        license: todo!(),
+    };
+    println!("Hello, world!");
+}
---
+++ sealcid/src/lib/server/app.rs
@@ -0,0 +1,7 @@
+pub struct App {
+    pub name: String,
+    pub version: String,
+    pub description: String,
+    pub author: String,
+    pub license: String,
+}
