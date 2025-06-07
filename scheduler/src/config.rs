c30ae43ae8ce7641bde59349d8bbbe97f636f8f0
Théo Tchilinguirian <theo.tchlx@proton.me>
2025-06-07 14:02:04 +0200

    feat(scheduler/config): switch default addr to IPv6
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@proton.me>

--- scheduler/src/config.rs
+++ scheduler/src/config.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 #[derive(Clone, Parser)]
 pub struct Config {
     /// The address to bind the gRPC server to
-    #[clap(short, long, default_value = "0.0.0.0:50051",
+    #[clap(short, long, default_value = "[::1]:50051",
             help = "The address to bind the gRPC server to")]
     pub addr: String,
 }
