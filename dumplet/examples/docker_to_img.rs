1f82b8ba919ab77adde6a0ae68350ae945173504
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-06-12 21:37:45 +0200

    fix:dumplet
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- dumplet/examples/docker_to_img.rs
+++ dumplet/examples/docker_to_img.rs
@@ -4,10 +4,11 @@ use dumplet::generate_initramfs_image;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let image = "ubuntu"; // Replace with your Docker image name
     let env: Option<Vec<&str>> = Some(vec!["test=test"]); // Adjust as needed
-    let args: Vec<String> =
-        vec!["/home/hugo/Bureau/cours/rust_sor/sealci/dumplet/src/main.rs:/app/caca/test.rs".to_string()]; // Adjust as needed
+    let args: Vec<String> = vec![
+        "/home/hugo/Bureau/cours/rust_sor/sealci/dumplet/src/main.rs:/app/caca/test.rs".to_string(),
+    ]; // Adjust as needed
 
-    let bundle = generate_initramfs_image(image, env, args).await?;
+    let bundle = generate_initramfs_image(image, env, args, Some("".to_string())).await?;
     println!("Bundle created successfully: {:?}", bundle);
     Ok(())
 }
