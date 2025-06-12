f1b5fe3cbb413b745f7d1e620bbc36d0b6a28c49
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2025-06-12 10:39:59 +0200

    feat: added release idk
    Signed-off-by: Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>

--- controller/src/lib/application/http/release/handlers/release.rs
+++ controller/src/lib/application/http/release/handlers/release.rs
@@ -1,8 +1,11 @@
-use actix_web::{post, web, HttpResponse, Responder};
+use actix_web::{error::InternalError, post, web, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
 use tracing::{error, info};
 
-use crate::application::app_context::AppContext;
+use crate::{
+    application::{app_context::AppContext, ports::release_service::ReleaseService},
+    domain::releases::entities::ReleaseError,
+};
 
 #[derive(Debug, Deserialize)]
 pub struct ReleaseRequest {
@@ -26,13 +29,45 @@ pub async fn handle_release(
         release_data.repo_url, release_data.tag_name
     );
 
+    let mut release_service = ctx.release_service.lock().await;
+    match release_service
+        .create_release(
+            release_data.repo_url.to_string().as_str(),
+            release_data.tag_name.to_string().as_str(),
+        )
+        .await
+    {
+        Ok(_) => HttpResponse::Ok().json(ReleaseResponse {
+            status: "success".to_string(),
+            message: format!(
+                "Release request received for tag {} on repository {}",
+                release_data.tag_name, release_data.repo_url
+            ),
+        }),
+        Err(ReleaseError::DatabaseError(e)) => {
+            HttpResponse::InternalServerError().json(ReleaseResponse {
+                status: "error".to_string(),
+                message: e.to_string(),
+            })
+        }
+        Err(ReleaseError::NotFound) => HttpResponse::NotFound().json(ReleaseResponse {
+            status: "error".to_string(),
+            message: "Release not found".to_string(),
+        }),
+        Err(ReleaseError::InternalError) => {
+            HttpResponse::InternalServerError().json(ReleaseResponse {
+                status: "error".to_string(),
+                message: "Internal server error :(".to_string(),
+            })
+        }
+        Err(ReleaseError::ReleaseAgentError) => {
+            HttpResponse::InternalServerError().json(ReleaseResponse {
+                status: "error".to_string(),
+                message: "Release agent error :(".to_string(),
+            })
+        }
+    }
+
     // TODO: Implement release handling logic
     // For now, we'll just acknowledge the request
-    HttpResponse::Ok().json(ReleaseResponse {
-        status: "success".to_string(),
-        message: format!(
-            "Release request received for tag {} on repository {}",
-            release_data.tag_name, release_data.repo_url
-        ),
-    })
 }
